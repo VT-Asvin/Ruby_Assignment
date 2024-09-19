73b68301922d612ecdbeb2dec2a150f026ecf0d7
VT-Asvin <asvin@sedintechnologies.com>
2024-09-19 10:12:33 +0530

    Update bubble_sort.rb
    modfied spaces

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,24 +1,15 @@
 def bubble_sort(arr)
-	
   n = arr.length
   loop do
     swap = false
-
     (n - 1).times do |j|
-
       if arr[j] > arr[j + 1]
         arr[j], arr[j + 1] = arr[j + 1], arr[j] 
         swap = true
       end
-
     end
-
     break if swap==false  
   end
   arr  
-
 end
-
-
-
 puts bubble_sort([17, 3, 6, 9, 15, 8])
