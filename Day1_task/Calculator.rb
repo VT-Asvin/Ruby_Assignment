abf6134a086344c821b34ba315114cb50a410c4b
VT-Asvin <asvin@sedintechnologies.com>
2024-09-20 12:48:36 +0530

    Update Calculator.rb
    Modifed redability

--- Day1_task/Calculator.rb
+++ Day1_task/Calculator.rb
@@ -4,7 +4,6 @@ class Calculator
     @num1 = num1
     @num2 = num2
   end
-
   def add
     @num1 + @num2
   end
@@ -33,23 +32,15 @@ class Calculator
   end
 end
 
-
 p "Enter num1"
 num1 = gets.chomp.to_i
 
 p "Enter num2"
 num2 = gets.chomp.to_i
-
-# Object creation
 calc = Calculator.new(num1, num2)
-
-
 puts "addition : #{ calc.add }"
 puts "subtraction : #{ calc.subtract }"
 puts "multiplication : #{ calc.multiply }"
 puts "division : #{ calc.divide }"
 puts "exponentiation : #{ calc.exponentiate }"
 puts "modulo : #{ calc.modulo }"
-puts "Num1: #{calc.disp}"
-puts "Num2: #{calc.disp}"
-"hau".length
